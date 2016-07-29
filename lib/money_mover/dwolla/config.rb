531c061514e671e91d7f0ff5858519c70daf23a3
Kevin Compton <klcompt@gmail.com>
2016-07-29 14:13:44 -0500

    Rename @config variable to @config_provider to avoid confusion.

--- lib/money_mover/dwolla/config.rb
+++ lib/money_mover/dwolla/config.rb
@@ -1,32 +1,32 @@
 module MoneyMover
   module Dwolla
     class Config
-      def initialize(config = MoneyMover::Dwolla.config_provider)
-        @config = config
+      def initialize(config_provider = MoneyMover::Dwolla.config_provider)
+        @config_provider = config_provider
       end
 
       def webhook_secret_key
-        @config.webhook_secret_key
+        @config_provider.webhook_secret_key
       end
 
       def webhook_callback_url
-        @config.webhook_callback_url
+        @config_provider.webhook_callback_url
       end
 
       def api_key
-        @config.api_key
+        @config_provider.api_key
       end
 
       def api_secret_key
-        @config.api_secret_key
+        @config_provider.api_secret_key
       end
 
       def environment
-        @config.environment
+        @config_provider.environment
       end
 
       def account_token_provider
-        @config.account_token_provider.new
+        @config_provider.account_token_provider.new
       end
     end
   end
